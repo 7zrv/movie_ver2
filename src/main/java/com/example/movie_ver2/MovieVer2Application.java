6fd266a39446debf056c857611938e67aeee6f3c
7zrv <4014133@gmail.com>
2023-07-28 21:47:20 +0900

    레포지토리 자동 생성.

---
+++ src/main/java/com/example/movie_ver2/MovieVer2Application.java
@@ -0,0 +1,13 @@
+package com.example.movie_ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieVer2Application.class, args);
+    }
+
+}
