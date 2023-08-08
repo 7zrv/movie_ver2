383a7a63693e835a04fadae5f20aac93773d3101
7zrv <4014133@gmail.com>
2023-08-08 18:35:14 +0900

    movie 초기 디렉토리 생성

--- src/main/java/com/example/movie_ver2/movie/entity/Movie.java
+++ src/main/java/com/example/movie_ver2/movie/entity/Movie.java
@@ -1,4 +1,42 @@
 package com.example.movie_ver2.movie.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "movies")
+
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "movie_id", nullable = false)
+    private Long id;
+
+
+    @CreatedDate
+    @Column(name = "signup_at")
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+
+
 }
+
+
