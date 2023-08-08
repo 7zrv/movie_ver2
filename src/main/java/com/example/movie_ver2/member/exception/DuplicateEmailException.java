e9f80ff848788fb0968e34abb111bfcd1ac19814
7zrv <4014133@gmail.com>
2023-08-08 18:07:52 +0900

    movie 초기 디렉토리 생성

--- src/main/java/com/example/movie_ver2/member/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package com.example.movie_ver2.member.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/movie_ver2/member/exception/NoSuchMemberException.java
+++
@@ -1,7 +0,0 @@
-package com.example.movie_ver2.member.exception;
-
-public class NoSuchMemberException extends RuntimeException {
-    public NoSuchMemberException(String message) {
-        super(message);
-    }
-}
