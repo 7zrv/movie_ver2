29493b1729d39742a00aefd5e209f2ea970aaae2
7zrv <4014133@gmail.com>
2023-08-31 21:38:32 +0900

    yml, test 클래스 수정

--- src/test/java/com/example/movie_ver2/MovieVer2ApplicationTests.java
+++ src/test/java/com/example/movie_ver2/MovieVer2ApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.movie_ver2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MovieVer2ApplicationTests {
 
     @Test
