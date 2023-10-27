427c5a4536eb87ecbd74c96e191c569aaf678954
dudujh0106 <rkdwlgh01@naver.com>
2023-10-28 05:07:27 +0900

    Review 페이징 구현

--- src/main/resources/static/js/page.js
+++ src/main/resources/static/js/page.js
@@ -6,7 +6,7 @@ function setPageNation(totalPage, currentPage, groupSize){
 
     if (groupLast > totalPage) groupLast = totalPage;
 
-    if(groupFirst != 1) pageHtml += `<a class="arrow pprev" href="#1"></a><a class="arrow prev" href="#${groupFirst-groupSize}"></a>`;
+    if(groupFirst !== 1) pageHtml += `<a class="arrow pprev" href="#1"></a><a class="arrow prev" href="#${groupFirst-groupSize}"></a>`;
     for(let i=groupFirst; i<=groupLast; i++) {
         if(i == currentPage){
             pageHtml += `<a href="#${i}" class="active">${i}</a>`
@@ -15,7 +15,7 @@ function setPageNation(totalPage, currentPage, groupSize){
             pageHtml += `<a href="#${i}">${i}</a>`;
         }
     }
-    if(groupLast != totalPage) pageHtml += `<a class="arrow next" href="#${groupFirst+groupSize}"></a><a class="arrow nnext" href="#${totalPage}"></a>`;
+    if(groupLast !== totalPage) pageHtml += `<a class="arrow next" href="#${groupFirst+groupSize}"></a><a class="arrow nnext" href="#${totalPage}"></a>`;
 
     $('.page_nation').html($(pageHtml));
 }
