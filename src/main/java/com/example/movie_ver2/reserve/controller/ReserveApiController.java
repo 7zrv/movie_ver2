6637102fa5cc5b0c2229fc7a1a08613f33dbae12
7zrv <4014133@gmail.com>
2023-12-19 23:39:27 +0900

    예매 기능 구현

---
+++ src/main/java/com/example/movie_ver2/reserve/controller/ReserveApiController.java
@@ -0,0 +1,13 @@
+
+package com.example.movie_ver2.reserve.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ReserveApiController {
+
+
+
+}
