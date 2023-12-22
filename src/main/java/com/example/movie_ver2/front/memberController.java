ee3b410ea80e5045b049c797f514bc3d10075a1e
7zrv <4014133@gmail.com>
2023-12-22 23:28:12 +0900

    회원관련, 예매관련 초본

--- src/main/java/com/example/movie_ver2/front/memberController.java
+++ src/main/java/com/example/movie_ver2/front/memberController.java
@@ -3,15 +3,28 @@ package com.example.movie_ver2.front;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class memberController {
 
 
-    @GetMapping("/signup")
+    @RequestMapping("/signup")
     public String showSignupPage(){
 
         return "memberHtml/signupPage";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage(){
+
+        return "memberHtml/login";
+    }
+
+    @GetMapping("/myPage")
+    public String showMyPage(){
+
+        return "memberHtml/myPage";
     }
 
 
