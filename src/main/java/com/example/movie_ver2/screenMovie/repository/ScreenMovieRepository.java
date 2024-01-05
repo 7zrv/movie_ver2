c438e920441fa22a598e472cc83f7ded108e9e20
7zrv <4014133@gmail.com>
2024-01-05 15:40:28 +0900

    상영영화별 극장 개수 카운팅 메서드 수정

--- src/main/java/com/example/movie_ver2/screenMovie/repository/ScreenMovieRepository.java
+++ src/main/java/com/example/movie_ver2/screenMovie/repository/ScreenMovieRepository.java
@@ -12,6 +12,8 @@ import java.util.List;
 public interface ScreenMovieRepository extends JpaRepository<ScreenMovies, Long> {
     //List<ScreenMovies> findByTheater(Theater theater);
     void deleteAllByMovieInAndTheater(List<Movie> movies, Theater theater);
+
+    Long countByTheater(Theater theater);
     //이미 있는 상영영화는 무시하고 없는 영화만 추가
     //List<ScreenMovies> findAllByMovieInAndTheater(List<Movie> movies, Theater theater);
 }
