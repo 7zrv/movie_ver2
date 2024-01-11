ddb0f12e78cf7e424b4dd227945a0764c9f126e0
7zrv <4014133@gmail.com>
2024-01-11 19:59:31 +0900

    s3 config, 영화 줄거리 길이제한 수정

--- src/main/java/com/example/movie_ver2/core/config/S3Config.java
+++ src/main/java/com/example/movie_ver2/core/config/S3Config.java
@@ -14,8 +14,10 @@ public class S3Config {
 
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
+
     @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
+
     @Value("${cloud.aws.region.static}")
     private String region;
 
