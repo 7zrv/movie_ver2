47e6bca69ee477ac80f3f9f608929c590a9d207c
dudujh0106 <rkdwlgh01@naver.com>
2024-01-19 15:50:52 +0900

    Theater Dto 수정(local 추가)

--- src/main/java/com/example/movie_ver2/theater/dto/TheaterAreaDto.java
+++ src/main/java/com/example/movie_ver2/theater/dto/TheaterAreaDto.java
@@ -9,17 +9,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TheaterAreaDto {
     private Long id;
+    private String local;
     private String area;
 
     @Builder
-    public TheaterAreaDto(Long id, String area) {
+    public TheaterAreaDto(Long id, String local, String area) {
         this.id = id;
+        this.local = local;
         this.area = area;
     }
 
     public static TheaterAreaDto of(Theater theater) {
         return TheaterAreaDto.builder()
                 .id(theater.getId())
+                .local(theater.getAddress().substring(0,2))
                 .area(theater.getArea())
                 .build();
     }
