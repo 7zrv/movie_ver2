8bbdd2696acdf10a71a8e6d9b1b818e98a36f6aa
dudujh0106 <rkdwlgh01@naver.com>
2024-01-23 15:45:42 +0900

    Hall Dto 수정(HallInfoDto-상영일정 id 목록 추가)

--- src/main/java/com/example/movie_ver2/hall/dto/HallInfoDto.java
+++ src/main/java/com/example/movie_ver2/hall/dto/HallInfoDto.java
@@ -1,9 +1,13 @@
 package com.example.movie_ver2.hall.dto;
 
 import com.example.movie_ver2.hall.entity.Hall;
+import com.example.movie_ver2.schedule.entity.Schedule;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -12,13 +16,15 @@ public class HallInfoDto {
     private String name;
     private Integer floor;
     private Integer seats;
+    private List<Long> schedules;
 
     @Builder
-    public HallInfoDto(Long id, String name, Integer floor, Integer seats){
+    public HallInfoDto(Long id, String name, Integer floor, Integer seats, List<Long> schedules){
         this.id = id;
         this.name = name;
         this.floor = floor;
         this.seats = seats;
+        this.schedules = schedules;
     }
 
     public static HallInfoDto of(Hall hall) {
@@ -27,6 +33,7 @@ public class HallInfoDto {
                 .name(hall.getName())
                 .floor(hall.getFloor())
                 .seats(hall.getSeats())
+                .schedules(hall.getSchedules().stream().map(Schedule::getId).collect(Collectors.toList()))
                 .build();
     }
 }
